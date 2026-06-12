3f2209527cd0a4a06f274cca8844d61839ea8745
Michael Nutt <michael@nuttnet.net>
2026-06-12 14:25:44 -0400

    track workerd npm staging manifest

--- cmake/SandstormInstall.cmake
+++ cmake/SandstormInstall.cmake
@@ -15,6 +15,7 @@ function(sandstorm_install_native)
 
   set(_workerd_dir "${CMAKE_BINARY_DIR}/workerd-npm")
   set(_workerd_bin "${CMAKE_BINARY_DIR}/bin/workerd")
+  set(_workerd_package_dir "${PROJECT_SOURCE_DIR}/deps/workerd-npm")
   if(SANDSTORM_WORKERD_BIN)
     add_custom_command(
       OUTPUT "${_workerd_bin}"
@@ -30,15 +31,21 @@ function(sandstorm_install_native)
       COMMAND "${CMAKE_COMMAND}" -E remove_directory "${_workerd_dir}"
       COMMAND "${CMAKE_COMMAND}" -E make_directory
         "${_workerd_dir}" "${CMAKE_BINARY_DIR}/bin"
+      COMMAND "${CMAKE_COMMAND}" -E copy
+        "${_workerd_package_dir}/package.json"
+        "${_workerd_package_dir}/package-lock.json"
+        "${_workerd_dir}"
       COMMAND "${CMAKE_COMMAND}" -E env
         "PATH=${SANDSTORM_METEOR_DEV_BUNDLE}/bin:$ENV{PATH}"
-        "${SANDSTORM_METEOR_DEV_BUNDLE}/bin/npm" install
-          --no-fund --no-save --prefix "${_workerd_dir}"
-          "workerd@${SANDSTORM_WORKERD_NPM_VERSION}"
+        "${SANDSTORM_METEOR_DEV_BUNDLE}/bin/npm" ci
+          --no-fund --prefix "${_workerd_dir}"
       COMMAND "${SANDSTORM_METEOR_DEV_BUNDLE}/bin/node" -e
         "const v=require('${_workerd_dir}/node_modules/workerd/package.json').version;if(v!=='${SANDSTORM_WORKERD_NPM_VERSION}')process.exit(1)"
       COMMAND "${CMAKE_COMMAND}" -E copy
         "${_workerd_dir}/node_modules/.bin/workerd" "${_workerd_bin}"
+      DEPENDS
+        "${_workerd_package_dir}/package.json"
+        "${_workerd_package_dir}/package-lock.json"
       COMMENT "Installing workerd from npm"
       VERBATIM)
   endif()
