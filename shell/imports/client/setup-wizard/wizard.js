13a977e04fbafca7ef9cc9a82fca153b0f2301ed
Michael Nutt <michael@nuttnet.net>
2026-08-02 19:37:34 -0400

    Own wizard style imports

--- shell/imports/client/setup-wizard/wizard.js
+++ shell/imports/client/setup-wizard/wizard.js
@@ -11,7 +11,12 @@ import { isDevelopmentServer } from "/imports/client/dev-mode";
 import downloadFile from "/imports/client/download-file";
 import { globalDb } from "/imports/db-deprecated";
 
-import "/imports/client/setup-wizard/styles/wizard.scss";
+import "/imports/client/setup-wizard/styles/_layout.scss";
+import "/imports/client/setup-wizard/styles/_progress.scss";
+import "/imports/client/setup-wizard/styles/_start.scss";
+import "/imports/client/setup-wizard/styles/_organization.scss";
+import "/imports/client/setup-wizard/styles/_preinstall.scss";
+import "/imports/client/setup-wizard/styles/_actions.scss";
 
 // Pseudocollection telling the client if there's an admin user yet.
 const HasAdmin = new Mongo.Collection("hasAdmin");
